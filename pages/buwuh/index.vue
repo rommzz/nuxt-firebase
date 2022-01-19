e1cf7657bea7050acea8f6e8c82d82f6906a632a
rommzz <rommy_zohara@yahoo.com>
2022-01-19 19:33:27 +0700

    pagination index

--- pages/buwuh/index.vue
+++ pages/buwuh/index.vue
@@ -1,7 +1,10 @@
 <template>
-	<div class="bg-gray-100 shadow-md p-2 rounded-md">
-    <div v-if="isLoading" class="text-2xl text-center">
-      <span class="animate-pulse">Memuat data...</span>
+	<div class="bg-gray-100 shadow-md p-2 rounded-md mb-10">
+    <div v-if="isLoading" class="flex justify-center text-2xl text-center">
+      <div class="mr-2">
+				<font-awesome-icon :icon="['fas', 'circle-notch']" class="animate-spin"/>
+			</div>
+			<span >Memuat data...</span>
     </div>
     <template v-else>
       <template v-if="items.length">
@@ -15,35 +18,40 @@
 						</Button>
 					</nuxt-link>
 				</div>
-				<div>
-					<template v-for="(item, index) in items">
-						<div :key="index" class="flex my-2">
-							<div class="w-full p-1 mr-2 pr-2 pl-2 bg-white rounded">
-								<div>
-									<span class="sm:text-3xl text-xl font-bold">
-										{{ item.data.name }}
-									</span>
+				<div >
+					<div>
+						<template v-for="(item, index) in items">
+							<div :key="index" class="flex my-2">
+								<div class="w-full p-1 mr-2 pr-2 pl-2 bg-white rounded">
+									<div>
+										<span class="sm:text-3xl text-xl font-bold">
+											{{ item.data.name }}
+										</span>
+									</div>
+									<div>
+										<span class="font-medium">
+											{{ item.data.value }}
+										</span>
+									</div>
 								</div>
-								<div>
-									<span class="font-medium">
-										{{ item.data.value }}
-									</span>
-								</div>
-							</div>
-							<div class="text-center">
-								<nuxt-link :to="{ name: 'buwuh-id', params: { id: item.id }}">
-									<Button fill-width color="green" label="Edit">
-										<font-awesome-icon :icon="['fas', 'edit']" class="mr-1"/>
+								<div class="text-center">
+									<nuxt-link :to="{ name: 'buwuh-id', params: { id: item.id }}">
+										<Button fill-width color="green" label="Edit">
+											<font-awesome-icon :icon="['fas', 'edit']" class="mr-1"/>
+										</Button>
+									</nuxt-link>
+									<div class="my-1" />
+									<Button fill-width color="red" label="Hapus" @click="deleteData(item)">
+										<font-awesome-icon :icon="['fas', 'trash-alt']" class="mr-1"/>
 									</Button>
-								</nuxt-link>
-								<div class="my-1" />
-								<Button fill-width color="red" label="Hapus" @click="deleteData(item)">
-									<font-awesome-icon :icon="['fas', 'trash-alt']" class="mr-1"/>
-								</Button>
+								</div>
 							</div>
-						</div>
-						<div v-if="index+1 < items.length" :key="'line'+index" class="my-2 bg-gray-300 h-0.5"/>
-					</template>
+							<div v-if="index+1 < items.length" :key="'line'+index" class="my-2 bg-gray-300 h-0.5"/>
+						</template>
+					</div>
+					<div v-if="hasLoadMore" class="text-center mt-4">
+						<Button :is-loading="isLoadMore" label="Muat Lebih" @click="loadMore()"/>
+					</div>
 				</div>
       </template>
       <div v-else class="text-center py-5">
@@ -63,7 +71,7 @@
 </template>
 
 <script>
-import { collection, query, where, getDocs, doc, deleteDoc } from 'firebase/firestore'
+import { collection, query, where, getDocs, doc, deleteDoc, orderBy, limit, startAfter } from 'firebase/firestore'
 import { db } from "~/plugins/firebase.js"
 import Button from '~/components/Button.vue'
 export default {
@@ -74,23 +82,31 @@ export default {
 			return {
 				items: [],
 				isLoading: false,
+				isLoadMore: false,
+				hasLoadMore: true,
+				limit: 3,
+				lastVisible: null
 			};
     },
     created() {
-        this.getData();
+      this.getData();
     },
     methods: {
 			async getData() {
 				this.isLoading = true;
-				const q = query(collection(db, "buwuh"), where("user_id", "==", this.$store.state.user.user.uid));
+				const q = query(collection(db, "buwuh"), where("user_id", "==", this.$store.state.user.user.uid), orderBy('name'), limit(this.limit));
 				const querySnapshot = await getDocs(q);
+				let count = 0
 				querySnapshot.forEach((doc) => {
 					// doc.data() is never undefined for query doc snapshots
 					this.items.push({
 						id: doc.id,
 						data: doc.data()
-					});
-				});
+					})
+					count++
+					this.lastVisible = doc
+				})
+				this.hasLoadMore = count === this.limit
 				this.isLoading = false;
 			},
 			edit(v) {
@@ -111,6 +127,27 @@ export default {
 					console.log(error);
 				}
 			},
+			async loadMore() {
+				this.isLoadMore = true
+				const loadMore = query(collection(db, "buwuh"),
+					where("user_id", "==", this.$store.state.user.user.uid),
+					orderBy('name'),
+					startAfter(this.lastVisible),
+					limit(this.limit));
+				let count = 0
+				const querySnapshot = await getDocs(loadMore);
+				querySnapshot.forEach((doc) => {
+					// doc.data() is never undefined for query doc snapshots
+					this.items.push({
+						id: doc.id,
+						data: doc.data()
+					})
+					count++
+					this.lastVisible = doc
+				})
+				this.hasLoadMore = count === this.limit
+				this.isLoadMore = false;
+			}
     }
 }
 </script>
