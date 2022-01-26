3ad694d4865892c381357cf5d4c76d2f6185436a
rommzz <rommy_zohara@gmail.com>
2022-01-26 11:32:04 +0700

    add icon to button register

--- pages/Register.vue
+++ pages/Register.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="">
-    <form class="shadow-lg rounded-md shadow-sm -space-y-px mx-auto max-w-screen-sm p-4 bg-gray-100" @submit.prevent>
+  <div class="shadow-lg rounded-md shadow-sm -space-y-px mx-auto max-w-screen-sm p-4 bg-gray-100">
+    <form class="" @submit.prevent>
       <div class="font-bold py-8 text-2xl text-center">
         REGISTER
       </div>
@@ -18,22 +18,24 @@
           <input id="passwordC" v-model="form.passwordRepeat" name="password" type="password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Konfrimasi password" />
         </div>
       </div>
-      <div class="flex justify-between py-4">
-        <div>
+      <div>
+        <div class="my-3">
+          <Button :is-loading="isLoading" label="Register" @click="submit()"/>
 					<nuxt-link to="/login">
-						<span class="hover:text-blue-500" >
-							Sudah Punya Akun?
-						</span>
+						<Button type="button" label="Login"/>
 					</nuxt-link>
         </div>
       </div>
-      <div>
-        <Button :isLoading="isLoading" class="min-w-full" label="Daftar" @click="submit()"/>
-				<div class="mt-2 bg-gray-400 mx-auto w-3/4" style="height: 1px;"/>
-        <Button class="min-w-full my-2" label="Masuk dengan Google"/>
-        <Button class="min-w-full" label="Masuk dengan Facebook "/>
-      </div>
     </form>
+    <div class="mt-2 bg-gray-400 mx-auto w-3/4" style="height: 1px;"/>
+    <div>
+      <Button class="min-w-full my-2" label="Masuk dengan Google" @click="submitGoogle()">
+        <font-awesome-icon :icon="['fab', 'google']" class="mr-1"/>
+      </Button>
+      <Button class="min-w-full"  label="Masuk dengan Facebook" @click="submitFb()">
+        <font-awesome-icon :icon="['fab', 'facebook-square']" class="mr-1"/>
+      </Button>
+    </div>
   </div>
 </template>
 
@@ -109,6 +111,14 @@ export default {
           });
       },
 			submit() {
+        if (!this.form.email || !this.form.password || !this.form.passwordRepeat) {
+          this.$toast.error('lengkapi form terlebih dahulu')
+          return
+        }
+        if (this.form.password !== this.form.passwordRepeat) {
+          this.$toast.error('password tidak cocok')
+          return
+        }
         this.isLoading = true
 				const auth = getAuth();
 				createUserWithEmailAndPassword(auth, this.form.email, this.form.password)
@@ -122,7 +132,9 @@ export default {
 						this.$router.push({ path: '/buwuh' })
 					})
 					.catch((error) => {
-						console.log(error);
+            if (error.code === 'auth/invalid-email') {
+             this.$toast.error('Alamat email tidak valid') 
+            }
 					}).finally(() => {
             this.isLoading = false
           })
